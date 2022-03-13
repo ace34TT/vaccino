e86c15e93edc66970df6b8b4e5069ff6ec91ec08
ace34TT <tafinasoa35@gmail.com>
2022-03-13 17:11:16 +0300

    login

--- application/views/dashboard/template.php
+++ application/views/dashboard/template.php
@@ -596,14 +596,14 @@
     <script src="<?php base_url() ?>assets/vendors/chart.js/Chart.min.js"></script>
     <!-- End plugin js for this page-->
     <!-- inject:js -->
-    <script src="js/off-canvas.js"></script>
-    <script src="js/hoverable-collapse.js"></script>
-    <script src="js/template.js"></script>
-    <script src="js/settings.js"></script>
-    <script src="js/todolist.js"></script>
+    <script src="<?= base_url() ?> assets/js/off-canvas.js"></script>
+    <script src="<?= base_url() ?> assets/js/hoverable-collapse.js"></script>
+    <script src="<?= base_url() ?> assets/js/template.js"></script>
+    <script src="<?= base_url() ?> assets/js/settings.js"></script>
+    <script src="<?= base_url() ?> assets/js/todolist.js"></script>
     <!-- endinject -->
     <!-- Custom js for this page-->
-    <script src="js/dashboard.js"></script>
+    <script src="<?= base_url() ?> assets/js/dashboard.js"></script>
     <!-- End custom js for this page-->
 </body>
 
